235a93a24b75fc93fda8020563186a3855f4e44b
José Naves <josenaves@gmail.com>
2016-04-03 17:32:40 -0300

    Add transition samples

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -48560,6 +48560,22 @@ module.exports = {
 var React = require('react');
 
 var About = React.createClass({displayName: "About",
+	statics: {
+		willTransitionTo: function(transition, params, query, callback) {
+			if (!confirm('Are you sure you read a page that\'s this boring?')) {
+				transition.abort();
+			} else {
+				callback();
+			}
+		},
+		willTransitionFrom: function(transition, component) {
+			if (!confirm('Are you sure you want to leave a page that\'s this exciting?')) {
+				transition.abort();
+			}
+		}
+
+	},
+
 	render: function() {
 		return (
 			React.createElement("div", null, 
@@ -48767,13 +48783,17 @@ var Router = require('react-router');
 var DefaultRoute = Router.DefaultRoute;
 var Route = Router.Route;
 var NotFoundRoute = Router.NotFoundRoute;
+var Redirect = Router.Redirect;
 
 var routes = (
 	React.createElement(Route, {name: "app", path: "/", handler: require('./components/app')}, 
 		React.createElement(DefaultRoute, {handler: require('./components/homePage')}), 
 		React.createElement(Route, {name: "authors", handler: require('./components/authors/authorPage')}), 
 		React.createElement(Route, {name: "about", handler: require('./components/about/aboutPage')}), 
-		React.createElement(NotFoundRoute, {handler: require('./components/notFoundPage')})
+		React.createElement(NotFoundRoute, {handler: require('./components/notFoundPage')}), 
+		React.createElement(Redirect, {from: "about-us", to: "about"}), 
+		React.createElement(Redirect, {from: "awthurs", to: "authors"}), 
+		React.createElement(Redirect, {from: "about/*", to: "about"})
 	)
 );
 
