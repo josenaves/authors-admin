309c83800676a7a5250e23f0f8744c16324ca9ed
José Naves <josenaves@gmail.com>
2016-04-02 21:17:06 -0300

    Initial commit

---
+++ src/api/authorData.js
@@ -0,0 +1,19 @@
+module.exports = {
+	authors : [
+		{
+			id: 'jorge-amado',
+			firstName: 'Jorge',
+			lastName: 'Amado'
+		},
+		{
+			id: 'frederick-forsyth',
+			firstName: 'Frederick',
+			lastName: 'Forsyth'
+		},
+		{
+			id: 'alexa-clay',
+			firstName: 'Alexa',
+			lastName: 'Clay'
+		}
+	]
+};
